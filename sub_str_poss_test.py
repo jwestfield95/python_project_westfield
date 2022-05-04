e021a8186713bcda682651d08d17604747b5f77a
jwestfield95 <102926730+jwestfield95@users.noreply.github.com>
2022-05-04 17:40:49 -0400

    Update sub_str_poss_test.py

--- sub_str_poss_test.py
+++ sub_str_poss_test.py
@@ -1,22 +1,50 @@
 #!/usr/bin/env python3
+from automated import *
 
-
-from sub_str_possbile import *
-   
-def pos_k():
-        assert k>0
-from observed_substrings import *
-
-def test_length_k():
-    len(observed_substrings) == k
-    expected= True 
-    assert actual_value== expected 
-def test_length_greater():
-    len(observed_substrings) > k
-    expected= False
-    assert actual_value == expected
+    #observed tests
+def test_observed_substring_k_is_9(): #specific test to make sure observed substrings for specific string is correct when k=9
+    result=observed_substrings('ATTTGGATT',9)
+    expected=1
+    assert result==expected
     
-def test_length_lesser():
-    len(observed_substrings) < k
-    expected= False
-    assert actual_value == expected
+def test_obsr_sub_k_is_10():
+    result=observed_substrings('ATTTGGATT',10)
+    expected=0
+    assert result==expected
+def test_obs_sub_k_is_14():
+    result=observed_substrings('ATATATATATATATATA',14)
+    expected=2
+    assert result==expected
+def test_obs_sub_k_is_30():
+    result=observed_substrings('ACTGCAGCGCGATGATGAGAGAGATTTCAGGACACACATTGCCAAATTGAGGCAT',30)
+    expected=26
+    assert result==expected
+# possible tests
+def test_possible_K_is_2():
+    result=sub_str_possible('ATTTGGATT',2)
+    expected=8
+    assert result==expected
+    
+def test_possible_k_is_7():
+    result=sub_str_possible('ATATATATATATATATA',7)
+    expected=11
+    assert result==expected
+def test_possible_k_is_17():
+    result=sub_str_possible('ATATATATATATATATA',17)
+    expected=1
+    assert result==expected
+    
+#Lingusitc complexity tests
+def test_ling_complex():
+    result=Ling_complx('ATATATATATATATATA')
+    expected=0.2357142857142857
+    assert result==expected
+def test_ling_complex_2nd_list():
+    result=Ling_complx('ATTTGGATT')
+    expected=0.875
+    assert result==expected
+
+def test_ling_complex_3rd_list():
+    result=Ling_complx('ACTGCAGCGCGATGATGAGAGAGATTTCAGGACACACATTGCCAAATTGAGGCAT')
+    expected=0.9738111647139903
+    assert result==expected
